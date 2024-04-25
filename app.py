a84f9ef8a268515fecfb7bc563bcf62f4003a407
DVIlangamge <163278504+DVIlangamge@users.noreply.github.com>
2024-04-25 18:57:28 +0000

    commit title

--- app.py
+++ app.py
@@ -2,3 +2,6 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 from mlxtend.frequent_patterns import association_rules, apriori
+
+st.title('''  Market Basket Analysis Results''')
+
