6e51917379ee9a83c97a37a3d23179c761573dfa
DVIlangamge <163278504+DVIlangamge@users.noreply.github.com>
2024-04-26 12:14:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,13 +2,15 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 from mlxtend.frequent_patterns import association_rules, apriori
+import plotly.express as px
+import seaborn as sns
 
 st.title('''  Market Basket Analysis Results''')
 
 #load dataset
 df = pd.read_excel('cleaned_dataset_global (1).xlsx')
 
-df["single_transaction"] = df["Customer ID"].astype(str)+'_'+df['Order Date'].astype(str)
+df["single_transaction"] = df["Customer ID"].astype(str)+'_'+df['Order Date'].astype(str) #do not display
 
 df1 = pd.crosstab(df['single_transaction'],df['Sub-Category'])
 
@@ -26,7 +28,7 @@ basket_input = df1.map(encode)
 frequent_itemsets = apriori(basket_input, min_support = 0.001, use_colnames=True)
 
 rules = association_rules(frequent_itemsets, metric = "lift", min_threshold=1)
-st.write(rules)
+
 ###### MBA using Segments to train the dataset
 ## For Segment 1
 
@@ -34,8 +36,6 @@ s1 = (df[df["Segment"] == "Consumer"]
      .groupby(["Order ID", "Sub-Category"])["Quantity"]
      .sum().unstack().reset_index().fillna(0)
      .set_index("Order ID"))
-s1
-
 
 def encode_units(x):
     if x <=0:
@@ -48,22 +48,40 @@ s1_sets = s1.map(encode_units)
 frequent_itemsets_s1 = apriori(s1_sets, min_support=0.001, use_colnames = True)
 rules_s1 = association_rules(frequent_itemsets_s1, metric = "lift", min_threshold=1)
 
-#Heat map for the whole dataset
+## For Segment 2
+s2 = (df[df["Segment"] == "Home Office"]
+     .groupby(["Order ID", "Sub-Category"])["Quantity"]
+     .sum().unstack().reset_index().fillna(0)
+     .set_index("Order ID"))
 
-st.subheader('''Recommended Items from the dataset''')
-heatmap_data = rules.pivot(index='antecedents', columns='consequents', values='lift')
-fig, ax = plt.subplots()
-ax.pcolor(heatmap_data)
-ax.set_xlabel('Consequents')
-ax.set_ylabel('Antecedents')
-#sns.heatmap(heatmap_data, annot=True, cmap='coolwarm', fmt=".2f", ax=ax)
-st.pyplot(fig)
+def encode_units(x):
+    if x <=0:
+        return 0
+    if x >=1:
+        return 1
 
-#options of Lift
-speed_options = ['1','2','3','4','5','6','7','8','9','10']
-Lift = st.select_slider("Lift Threshold", options=speed_options)
+s2_sets = s2.applymap(encode_units)
+
+frequent_itemsets_s2 = apriori(s2_sets, min_support=0.001, use_colnames = True)
+rules_s2 = association_rules(frequent_itemsets_s2, metric = "lift", min_threshold=1)
+
+## For Segment 3
+
+s3 = (df[df["Segment"] == "Corporate"]
+     .groupby(["Order ID", "Sub-Category"])["Quantity"]
+     .sum().unstack().reset_index().fillna(0)
+     .set_index("Order ID"))
 
 
-Segment = df['Segment'].unique()
-selected_column2 = st.selectbox("Select Segment :", Segment)
+def encode_units(x):
+    if x <=0:
+        return 0
+    if x >=1:
+        return 1
+
+s3_sets = s3.applymap(encode_units)
+
+frequent_itemsets_s3 = apriori(s3_sets, min_support=0.001, use_colnames = True)
+rules_s3 = association_rules(frequent_itemsets_s3, metric = "lift", min_threshold=1)
+
 
